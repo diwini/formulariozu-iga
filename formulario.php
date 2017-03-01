dfcbae1cc2232d405ee04680b9939b5a7d1be7e6
diwini <joaquin.bernal.h@gmail.com>
2017-02-28 23:09:17 -0300

    miha

--- formulario.php
+++ formulario.php
@@ -11,7 +11,7 @@
 	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="js/materialize.min.js"></script>
-	<input type="submit" value="Base de datos" onclick = "location='formulario.php'"/>
+	<input type="submit" value="Base de datos" onclick = "location='lista.php'"/>
  <div class="row">
     <form method="post" action="insertformulario.php" class="col s10">
       <div class="row">
