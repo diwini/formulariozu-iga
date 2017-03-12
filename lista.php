e5ec65d49a55468ca292bc9cf9e8544e490c14d4
Sikeze <mipozarski@alumnos.uai.cl>
2017-03-12 18:39:07 -0300

    buscador lista.php

--- lista.php
+++ lista.php
@@ -8,6 +8,44 @@
 <?php require_once 'conexionlocal.php';?>
 <!-- Conexion BBDD -->
 <!-- Inicio Contenido -->
+<?php 
+	$select = $_POST['select'];
+
+	$input = $_POST['input'];
+	if($select == null){
+		$select = -1;
+	}
+?>
+<div class="row">
+<h5>Buscador</h5>
+<form method="post" action="lista.php" class="col s12">
+      <div class="row">
+        <div class="input-field col s4">
+    <select name="select" id="buscadorselect">
+      <option value="" disabled selected>Seleccione una columna</option>
+      <option value="1">Nombre</option>
+      <option value="2">Empresa</option>
+      <option value="3">Requerimiento</option>
+      <option value="4">Asignacion</option>
+      <option value="5">Fecha recepcion</option>
+      <option value="6">Fecha compromiso</option>
+      <option value="7">Fecha cierre</option>
+      <option value="8">Observacion</option>
+      <option value="9">Rut</option>
+    </select>
+  </div>
+        <div id="tipoinput"class="input-field col s4">
+          <input id="last_name" type="text" class="validate" disabled>
+          <label for="last_name">Seleccione columna</label>
+        </div>
+       <button class="btn waves-effect waves-light" type="submit" name="action">Buscar
+    <i class="material-icons right">send</i>
+  </button>
+  </form>
+</div>
+</div>
+
+<div class= "row">
 <table class="striped">
         <thead>
           <tr>
@@ -33,7 +71,11 @@
 
         <tbody>
         <!-- Funcion trae data de la tabla -->
-        <?php get_cliente_data($con); ?>
+        <?php 
+        if($select == -1){
+        get_cliente_data($con);
+        }
+		?>
         <!-- Funcion trae data de la tabla -->
         </tbody>
       </table>
@@ -41,3 +83,24 @@
 <!-- Inicio footer -->
 <?php require_once 'footer.php';?>
 <!-- Final footer -->
+<script>
+$(document).ready(function() {
+    $('select').material_select();
+    $('.datepicker').pickadate({
+	    selectMonths: true, // Creates a dropdown to control month
+	    selectYears: 15, // Creates a dropdown of 15 years to control year
+	    format: 'dd-mm-yyyy'
+	  });
+    $('#buscadorselect').on('change', function() {
+    	if(this.value == 1 || this.value == 2 || this.value == 4 || this.value == 9){
+        $('#tipoinput').html("<input placeholder='Ingrese Texto' id='cliente' type='text' class='validate' name='input' required>");
+    	}
+    	if(this.value == 3 || this.value == 8){
+            $('#tipoinput').html("<textarea placeholder='Ingrese Texto' id='obs' class='materialize-textarea' name='input' required></textarea>");
+        }
+    	if(this.value == 5 || this.value == 6 || this.value == 7){
+    		$('#tipoinput').html("<input placeholder='Click Aqui' type='date' class='datepicker' name='input' required><script>$('.datepicker').pickadate({selectMonths: true, selectYears: 15, format: 'dd-mm-yyyy'});");
+    	}
+    })
+  });
+</script>
